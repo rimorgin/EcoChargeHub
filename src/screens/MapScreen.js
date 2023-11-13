b09b74464111e7d9bc1fb5154855183c3febe00b
rimorgin <calvscalderonx@gmail.com>
2023-11-14 00:15:16 +0800

    add screens

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function MapScreen() {
+export default function MapScreen({navigation}) {
   return (
     <View>
       <Text>MapScreen</Text>
+      <Text>MapScreen</Text>
+      <Text>MapScreen</Text>
+      <Button title='open drawer' onPress={() => navigation.openDrawer()}/>
     </View>
   )
 }
